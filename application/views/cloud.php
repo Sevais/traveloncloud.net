25023dfa5c38809e83fb6cda5318b53ca4eee3de
egemenozkan@gmail.com <egemenozkan@gmail.com>
2014-12-19 14:34:12 +0200

    update

--- application/views/cloud.php
+++ application/views/cloud.php
@@ -1,48 +1,46 @@
 <section class="container-fluid" id="section1">
   	<div class="container">
+  		
       <div class="row">
           <div class="col-sm-12">
+					<?php
+					$dir_list = explode('/', $dir);
+					$count = 0;
+					foreach ($dir_list as $key => $value) {
+						$breadcrumb = "";
+
+						if ($count > 1) {
+							for ($i = 1; $i <= $count; $i++) {
+								$breadcrumb .= $dir_list[$i] . '/';
+
+							}
+							echo '&nbsp;&raquo;&nbsp;<a href="' . base_url('fs?dir=' . $breadcrumb) . '">' . $value . '</a>';
+						}
+
+						$count++;
+					}
+					?>
+					<?php
+					/*
+					 if ($location = substr(dirname($dir), 1))
+					 $dirlist = explode('/', $location);
+					 else
+					 $dirlist = array();
+
+					 $count = array_push($dirlist, $dir);
+
+					 echo '<a href="' . $address . '">Home</a>';
+
+					 for ($i = 0; $i < $count; $i++)
+					 echo '&nbsp;&raquo;&nbsp;<a href="' . base_url('fs?dir=' . $dirlist[$i]) . '">' . $dirlist[$i] . '</a>';
+					 */
+	?>
+			</div>
+			</div>
+			<div class="row">
+          <div class="col-sm-12">
             	<table class="table" id="dirTable">
-            		<thead>
-            			<tr>
-            				<th colspan="5">
-            					<?php
-								$dir_list = explode('/', $dir);
-								$count = 0;
-								 foreach ($dir_list as $key => $value) {
-								 	$breadcrumb = "";
-								 	
-								
-								 	if($count>1){
-								 		for ($i=1; $i <= $count; $i++) { 
-										 $breadcrumb .= $dir_list[$i].'/';
-											
-									 }
-										 echo '&nbsp;&raquo;&nbsp;<a href="'.base_url('fs?dir='.$breadcrumb).'">'.$value.'</a>';
-								 	}
-										
-									
-									$count++;
-								 }
-            					?>
-            					<?php
-            					/*
-								if ($location = substr(dirname($dir), 1))
-									$dirlist = explode('/', $location);
-								else
-									$dirlist = array();
-
-								$count = array_push($dirlist, $dir);
-
-							
-
-								echo '<a href="' . $address . '">Home</a>';
-
-								for ($i = 0; $i < $count; $i++)
-									echo '&nbsp;&raquo;&nbsp;<a href="' . base_url('fs?dir=' . $dirlist[$i]) . '">' . $dirlist[$i] . '</a>';
-						*/	?> 
-            					</th>
-            			</tr>
+            		<thead>           			
             			<tr>
             				<th></th><th>Name</th><th>Type</th><th>Size</th><th>Creation Date</th>
             			</tr>
@@ -58,12 +56,11 @@
 									?>
 								<tr>
 									<?php
-									if (strpos($value['name'],'.PLAN') !== false) {
+									if (strpos($value['name'], '.PLAN') !== false) {
 										$file = "";
 										$name = $value['name'];
-									    $type = 'dir';
-									}
-									else{
+										$type = 'dir';
+									} else {
 										$file = explode('.', $value['name']);
 										$name = $file[0];
 										$type = (count($file) > 1) ? $file[1] : 'dir';
@@ -103,22 +100,21 @@
 										
 									</th>
 									<td><?php
-										if(strcmp('jpg',$type)==0 || strcmp('tif',$type)==0 || strcmp('png',$type)==0 || strcmp('jpeg',$type)==0 || strcmp('gif',$type)==0)
-											echo '<i class="fa fa-file-image-o"></i>';
-										elseif(strcmp('zip',$type)==0 || strcmp('rar',$type)==0)
-											echo '<i class="fa fa-file-archive-o"></i>';
-										elseif(strcmp('pdf',$type)==0)
-											echo '<i class="fa fa-file-pdf-o"></i>';
-										elseif( strcmp('xls',$type)==0 || strcmp('xlsx',$type)==0)
-											echo '<i class="fa fa-file-excel-o"></i>';
-										elseif( strcmp('doc',$type)==0 || strcmp('docx',$type)==0)
-											echo '<i class="fa fa-file-word-o"></i>';
-										elseif(strcmp('dir',$type)==0)
-											echo '<i class="fa fa-folder"></i>';
-										else {
-											echo $type;
-										}
-									
+									if (strcmp('jpg', $type) == 0 || strcmp('tif', $type) == 0 || strcmp('png', $type) == 0 || strcmp('jpeg', $type) == 0 || strcmp('JPG', $type) == 0 || strcmp('JPEG', $type) == 0 || strcmp('gif', $type) == 0)
+										echo '<i class="fa fa-file-image-o"></i>';
+									elseif (strcmp('zip', $type) == 0 || strcmp('rar', $type) == 0)
+										echo '<i class="fa fa-file-archive-o"></i>';
+									elseif (strcmp('pdf', $type) == 0)
+										echo '<i class="fa fa-file-pdf-o"></i>';
+									elseif (strcmp('xls', $type) == 0 || strcmp('xlsx', $type) == 0)
+										echo '<i class="fa fa-file-excel-o"></i>';
+									elseif (strcmp('doc', $type) == 0 || strcmp('docx', $type) == 0)
+										echo '<i class="fa fa-file-word-o"></i>';
+									elseif (strcmp('dir', $type) == 0)
+										echo '<i class="fa fa-folder"></i>';
+									else {
+										echo $type;
+									}
 									 ?>
 									 </td>
 									<td><?=($value['size'] > 4096) ? number_format((float)($value['size'] / 1048576), 2) : '-' ?> <?=($value['size'] > 4096) ? 'MB' : '' ?></td>
@@ -127,8 +123,8 @@
 								</tr>
 									<?php
 									endforeach;
-								
-								}
+
+									}
 								?>
             				
             			</tr>
